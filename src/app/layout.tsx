7ae659ecec379787c7e7730c1398fe2610d9b0da
Abiyyu Faisal Akmal <faisalakmal2105@gmail.com>
2026-03-13 23:44:12 +0700

    Clean up whitespace in layout.tsx
    Removed unnecessary whitespace in layout.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
 const americanCaptain = localFont({
   src: "../../public/font/American Captain.ttf",
   variable: "--font-american-captain",
 });
-
 export const metadata: Metadata = {
   title: "Personal Website",
   description: "Created with Next.js and Tailwind CSS",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
